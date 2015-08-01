6fc50d23ec39b24797575687143824a2ba90dbb6
Andrey Laguta <l.a.evil.genius@gmail.com>
2015-08-01 17:12:58 +0300

    minor fixes

--- runserver.bat
+++ runserver.bat
@@ -1,4 +1,3 @@
-set babel=C:\Users\"Evil Genius"\AppData\Roaming\npm\babel.cmd
-start %babel% js\planetarium.es6 --watch --out-file js\planetarium.js
-start %babel% js\planetarium-element.es6 --watch --out-file js\planetarium-element.js
+start babel js\planetarium.es6 --watch --out-file js\planetarium.js
+start babel js\planetarium-element.es6 --watch --out-file js\planetarium-element.js
 python -m http.server
